40b3f151ea1707d0ac1740746c5882bd49bfc31c
RaySunwar <ray@whitehatagency.com.au>
2020-11-02 13:32:11 +0545

    build: add styled-components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+const { default: Dashboard } = require("./components/dashboard/Dashboard");
+
 function App() {
   return (
     <div className="App">
+      <Dashboard />
     </div>
   );
 };
---
+++ src/components/dashboard/Dashboard.js
@@ -0,0 +1,10 @@
+import React, { Fragment } from "react";
+function Dashboard(){
+    return(
+        <Fragment>
+            
+        </Fragment>
+    );
+};
+
+export default Dashboard;
