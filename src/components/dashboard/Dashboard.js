fd1d793b7ad6a6dfc7e2bfc025c7c6916f957dfb
RaySunwar <ray@whitehatagency.com.au>
2020-11-24 09:07:57 +0545

    refactor: change case

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,65 +1,66 @@
 import React, { Fragment } from "react";
-import Styled from "styled-components";
+import styled from "styled-components";
 import Header from "../header/Header";
 import Featured from "./Featured";
 import LatestArticles from "./LatestArticles";
 import Image0 from "./../images/image0.jpg";
 
-const Nav = Styled.div`
+const Nav = styled.div`
   background-color: #ffffff;
   margin: 15px;
   padding: 0;
   max-width: 100%;
 `;
 
-const Subnav = Styled.div`
+const Subnav = styled.div`
+  margin-right: auto;
+  margin-left: auto;
   display: flex;
   font-family: "Mulish", sans-serif;
 `;
 
-const NavLeft = Styled.div`
+const NavLeft = styled.div`
   margin-left: 2.5rem;
   margin-right: 1.5rem;
+  position: relative;
   text-align: left;
-  width: 50%;
+  max-width: 50%;
   &::after {
-    transform: scale(1.25);
-    left: 25%;
-    bottom: -120px;
+    background-color: #f2f3f3;
     border-top-left-radius: 63% 70%;
     border-top-right-radius: 37% 68%;
     border-bottom-right-radius: 50% 32%;
     border-bottom-left-radius: 50% 30%;
-
     background: rgba(66,72,81,0.1);
-    background-color: #f2f3f3;
-    display: block; 
-    position: absolute;
-    height: 540px;
-    width: 540px;
     content: "";
+    display: block; 
+    height: 540px;
+    left: 25%;
+    position: absolute;
+    top: 0;
+    width: 540px;
     z-index: -1;
   };
 `;
 
-const NavRight = Styled.div`
+const NavRight = styled.div`
   display: block;
   margin-right: 4rem;
   padding-top: 2.5rem;  
   width: 50%; 
 `;
 
-const Title = Styled.h1`
+const Title = styled.h1`
+  box-sizing: inherit;
   color: #424851;
   font-weight: 900;
   font-size: 4.2rem;
   margin-left: 0;
   padding-top: 3rem;
   text-align: left;
-  box-sizing: inherit;
 `;
 
-const Content = Styled.p`
+const Content = styled.p`
   box-sizing: inherit;
   color: #424851;
   font-size: 1.2rem;
@@ -67,13 +68,13 @@ const Content = Styled.p`
   margin-top: 0;
 `;
 
-const Button = Styled.p`
+const Button = styled.p`
   box-sizing: inherit;
   font-size: 1rem;
   margin-top: 0;
 `;
 
-const Anchor = Styled.a`
+const Anchor = styled.a`
   background: #f13b3b;
   box-sizing: border-box;
   box-shadow: 0 10px 15px 0 rgba(241,59,59,0.2);
@@ -82,24 +83,24 @@ const Anchor = Styled.a`
   font-size: 1.2rem;
   font-weight: bold;
   float: none;
+  padding:  5px 20px 5px 20px;
   position: static;
   text-decoration: none;
-  padding:  5px 20px 5px 20px;
   z-index: auto;
 `;
 
-const FirstImg = Styled.img`
+const FirstImg = styled.img`
   border: 0;
-  object-fit: cover;
   border-top-left-radius: 63% 70%;
   border-top-right-radius: 37% 68%;
   border-bottom-right-radius: 50% 32%;
   border-bottom-left-radius: 50% 30%;
   height: 540px;
+  object-fit: cover;
   width: 540px; 
 `;
 
-function Dashboard() {
+function Dashboard(){
   return(
     <Fragment>
       <Nav>
